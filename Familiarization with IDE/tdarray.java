274b84798d398ab4873aa1a01fabea23c656441f
Giridhar Shanbhag <39093579+DudeL69@users.noreply.github.com>
2020-01-21 18:38:26 +0530

    Update tdarray.java

--- Familiarization with IDE/tdarray.java
+++ Familiarization with IDE/tdarray.java
@@ -1,29 +1,43 @@
 import java.util.*;
 
 public class tdarray{
-      public static void main(String[] args){
-            Scanner s= new Scanner(System.in);
-            System.out.print("Enter the order of the matrix: ");
-            int m = s.nextInt();
-            int n = s.nextInt();
-            int mat[][]=new int[m][n];
-            int flag=1;
-            System.out.print("Enter the "+m*n+" elements in increasing order:\n");
-            for(int i=0;i<m;i++)
-                  for(int j=0;j<n;j++)
-                        mat[i][j]=s.nextInt();
-            System.out.print("Enter the key to be searched: ");
-            int key=s.nextInt();
-            for(int i=0;i<m;i++)
-                  for(int j=0;j<n;j++)
-                        if(mat[i][j]==key)
-                        {
-                              int k=i+1,l=j+1;
-                              System.out.print("The element "+key+" is found at row "+k+" and column "+l+".");
-                              flag=0;
-                              break;
-                        }
-            if(flag==1)
-                        System.out.print("Element "+key+" not found in the 2-D array...");
-      }
+	public static void main(String[] args){
+		Scanner s= new Scanner(System.in);
+		int m,n;
+		System.out.print("Enter the order of the matrix: ");
+		m=s.nextInt();
+		n=s.nextInt();
+		int mat[][]=new int[m][n];
+		System.out.print("Enter the elements of the matrix:\n");
+		for(int i=0;i<m;i++)
+			for(int j=0;j<n;j++)
+				mat[i][j]=s.nextInt();
+		System.out.print("Enter the element to be searched: ");
+		int key=s.nextInt();
+		int rindex=0,cindex=0;
+		for(int i=0;i<m-1;i++)
+		{
+			if(key>mat[i][0]&&key<mat[i+1][0])
+			{
+				rindex=i;
+				break;
+			}
+			else if(key==mat[i][0])
+			{
+				rindex=i;
+				break;
+			}
+		}
+		for(int j=0;j<n;j++)
+		{
+			if(key==mat[rindex][j])
+			{
+				cindex=j;
+				break;
+			}
+		}
+		int row=rindex+1,col=cindex+1;
+		if(mat[rindex][cindex]==key)
+			System.out.print("Row: "+row+"\nCol: "+col+"\n");
+	}
 }

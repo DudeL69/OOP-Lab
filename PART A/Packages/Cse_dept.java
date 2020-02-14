190b9700f6db5ecaddbf2e4093bb7a2e6ea46b26
Giridhar Shanbhag <geeks2022@gmail.com>
2020-02-14 19:19:18 +0530

    Add files via upload

---
+++ PART A/Packages/Cse_dept.java
@@ -0,0 +1,10 @@
+package RVCE;
+import java.util.*;
+import CSE.*;
+
+public class Cse_dept {
+    public static void main(){
+        Thirdsem t=new Thirdsem();
+        t.welcomemsg();
+    }
+}
---
+++ PART A/Packages/Thirdsem.java
@@ -0,0 +1,8 @@
+package CSE;
+import java.util.*;
+
+public class Thirdsem {
+    public void welcomemsg(){
+        System.out.print("Welcome to CSE dept-3rd Sem young budding Engineers...");
+    }
+}

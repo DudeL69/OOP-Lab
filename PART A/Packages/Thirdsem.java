e514e1e0adfd781377caf5ccc995cb64067cb3ac
Giridhar Shanbhag <geeks2022@gmail.com>
2020-03-02 22:30:18 +0530

    Delete Thirdsem.java

--- PART A/Packages/Thirdsem.java
+++
@@ -1,8 +0,0 @@
-package CSE;
-import java.util.*;
-
-public class Thirdsem {
-    public void welcomemsg(){
-        System.out.print("Welcome to CSE dept-3rd Sem young budding Engineers...");
-    }
-}
